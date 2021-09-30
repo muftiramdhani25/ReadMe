654316c6408e1ca785527ddc74abd2a60b03328e
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-09-30 17:57:06 +0700

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Mufti Ramdhani on 30/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
